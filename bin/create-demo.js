39619e812bd5def565570400f14adcd8d7c85f39
Elizabeth Parnell <elizabethparnell@Elizabeths-Mac-mini.local>
2023-09-22 14:32:34 -0400

    removed solutions from main branch

--- bin/create-demo.js
+++
@@ -1,6 +0,0 @@
-// load('./bin/create-demo.js')
-
-db.tng.insertOne({
-    name: 'Jean-Luc Picard',
-    rank: 'Captain'
-})
--- bin/show.js
+++
@@ -1,2 +0,0 @@
-// load('./bin/show.js')
-console.log(db.tng.findOne({name: 'Beverly Crusher'}))
